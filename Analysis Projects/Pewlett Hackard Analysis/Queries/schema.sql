3e4e4d0eb9061d3e3a849fedc3c14e7a9a68d832
natalia velasquez <nataliavelasquez@natalias-MacBook-Pro.local>
2021-01-10 13:18:04 -0500

    adding finished SQL challenge

--- Analysis Projects/Pewlett Hackard Analysis/Queries/schema.sql
+++ Analysis Projects/Pewlett Hackard Analysis/Queries/schema.sql
@@ -1,58 +1,41 @@
--- Creating tables for PH-EmployeeDB
-CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
-     dept_name VARCHAR(40) NOT NULL,
-     PRIMARY KEY (dept_no),
-     UNIQUE (dept_name)
-);
+Departments
+-
+dept_no varchar pk FK - dept_managers.dept_no
+dept_name varchar
 
-CREATE TABLE employees (
-	    emp_no INT NOT NULL,
-     birth_date DATE NOT NULL,
-     first_name VARCHAR NOT NULL,
-     last_name VARCHAR NOT NULL,
-     gender VARCHAR NOT NULL,
-     hire_date DATE NOT NULL,
-     PRIMARY KEY (emp_no)
-);
+Employees
+-
+emp_no int pk FK - dept_managers.emp_no
+birth_date date
+first_name varchar
+last_name varchar
+gender varchar
+hire_date date
 
-CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
-);
+dept_managers
+-
+dept_no varchar pk
+emp_no  int fk
+from_date date
+to_date date
 
-CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
+dept_emp
+-
+emp_no int pk fk >- Employees.emp_no
+dept_no varchar pk FK >- Departments.dept_no
+from_date date
+to_date date
 
-CREATE TABLE dept_employees (
-	emp_no INT 
-	birth_date DATE
-	first_name VARCHAR(30) NOT NULL,
-	last_name VARCHAR(30) NOT NULL,
-	gender VARCHAR(20) NOT NULL,
-	hire_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
-	
-);
+Salaries
+-
+emp_no int pk FK - Employees.emp_no
+salary int
+from_date date
+to_date date
 
-
-CREATE TABLE titles (
-	emp_no INT, 
-	title VARCHAR(40),
-	from_date DATE,
-	to_date DATE,
-	PRIMARY KEY (emp_no)
-	
-	
-);
+Titles
+-
+emp_no int pk fk >- Employees.emp_no
+title varchar pk
+from_date date pk
+to_date date
